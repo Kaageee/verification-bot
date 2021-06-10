5eae2542fe3f655f7521e1c8aa73a3c10e9bb01f
Kaageee <65356524+Kaageee@users.noreply.github.com>
2021-06-10 23:57:45 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import settings
+import dontedit
+
+#####################################################################################   
+##      $$\   $$\                                                                  ##
+##      $$ | $$  |                                                                 ##
+##      $$ |$$  / $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\        ##
+##      $$$$$  /  \____$$\  \____$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\       ##
+##      $$  $$<   $$$$$$$ | $$$$$$$ |$$ /  $$ |$$$$$$$$ |$$$$$$$$ |$$$$$$$$ |      ##
+##      $$ |\$$\ $$  __$$ |$$  __$$ |$$ |  $$ |$$   ____|$$   ____|$$   ____|      ##
+##      $$ | \$$\\$$$$$$$ |\$$$$$$$ |\$$$$$$$ |\$$$$$$$\ \$$$$$$$\ \$$$$$$$\       ##
+##      \__|  \__|\_______| \_______| \____$$ | \_______| \_______| \_______|      ##
+##                                   $$\   $$ |                                    ##
+##                                   \$$$$$$  |                                    ##
+##                                    \______/                                     ##
+#####################################################################################
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix=settings.Prefix, help_command=None, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(dontedit.sn)
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(settings.BotStatus))
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    if message_id == int(settings.MessageId):
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+        if payload.emoji.name == settings.Reaction:
+            user = payload.member
+            role = discord.utils.get(user.guild.roles, name=settings.rolename)
+            await user.add_roles(role)
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("test")
+
+bot.run(settings.TOKEN)
---
+++ settings.py
@@ -0,0 +1,23 @@
+#################################################
+##     _____      _   _   _                    ##
+##    /  ___|    | | | | (_)                   ##
+##    \ `--.  ___| |_| |_ _ _ __   __ _ ___    ##
+##     `--. \/ _ \ __| __| | '_ \ / _` / __|   ##
+##    /\__/ /  __/ |_| |_| | | | | (_| \__ \   ##
+##    \____/ \___|\__|\__|_|_| |_|\__, |___/   ##
+##                                 __/ |       ##
+##                                |___/        ##
+#################################################
+
+#Here is your bot's TOKEN
+TOKEN = ''
+#The status of your bot
+BotStatus = ""
+#The prefix for the bot's commands
+Prefix = "."
+#The reactions the peoples have to add to get the role
+Reaction = ""
+#The id of the message for the reaction
+MessageId = ""
+#The name of the role you want to add with the reaction
+rolename = ""
